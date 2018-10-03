1c09424c52c115376ac310ca05db94b9bd55b003
akraskovski <artem.kraskovski@gmail.com>
2018-10-03 22:29:36 +0300

    refactoring

--- multi-module/resource-server/src/main/java/com/github/akraskovski/resource/server/application/config/ResourceServerConfig.java
+++ multi-module/resource-server/src/main/java/com/github/akraskovski/resource/server/application/config/ResourceServerConfig.java
@@ -1,34 +1,34 @@
-//package com.github.akraskovski.resource.server.application.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-//import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
-//
-///**
-// * Configuration this module as an OAuth2.0 Resource Server instance.
-// */
-//@Configuration
-//@EnableResourceServer
-//@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-//public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-//
-//    private static final String RESOURCE_ID = "company_api";
-//
-//    @Override
-//    public void configure(final ResourceServerSecurityConfigurer resources) {
-//        resources.resourceId(RESOURCE_ID);
-//    }
-//
-//    @Override
-//    public void configure(final HttpSecurity http) throws Exception {
-//        http
-//                .antMatcher("/api/**").authorizeRequests().anyRequest().permitAll()
-////                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')")
-//                .and()
-//                .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
-//    }
-//}
+package com.github.akraskovski.resource.server.application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+
+/**
+ * Configuration this module as an OAuth2.0 Resource Server instance.
+ */
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private static final String RESOURCE_ID = "company_api";
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID);
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/api/**").authorizeRequests().anyRequest().permitAll()
+//                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')")
+                .and()
+                .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+    }
+}
