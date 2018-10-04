b87022e8ed7fe24f279e91f3898fca7c9bf75b67
akraskovski <artem.kraskovski@gmail.com>
2018-10-04 20:25:25 +0300

    starting basic security templates

---
+++ basic-security/src/main/java/com/github/akraskovski/basic/security/BasicSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.github.akraskovski.basic.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSecurityApplication.class, args);
+    }
+}
