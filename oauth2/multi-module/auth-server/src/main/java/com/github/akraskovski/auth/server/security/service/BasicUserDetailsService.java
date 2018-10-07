8f09905fc2ce3e618375042d2b49f294acc4ee86
akraskovski <artem.kraskovski@gmail.com>
2018-10-07 14:26:48 +0300

    making oauth2 multi-module working

--- oauth2/multi-module/auth-server/src/main/java/com/github/akraskovski/auth/server/security/service/BasicUserDetailsService.java
+++ oauth2/multi-module/auth-server/src/main/java/com/github/akraskovski/auth/server/security/service/BasicUserDetailsService.java
@@ -1,13 +1,18 @@
 package com.github.akraskovski.auth.server.security.service;
 
 import com.github.akraskovski.auth.server.domain.model.DomainUserDetails;
+import com.github.akraskovski.auth.server.domain.model.UserRole;
 import com.github.akraskovski.auth.server.domain.repository.UserDetailsRepository;
-import com.github.akraskovski.auth.server.security.model.ApplicationUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Implementation of the Spring Security {@link UserDetailsService}.
@@ -23,6 +28,13 @@ public class BasicUserDetailsService implements UserDetailsService {
         final DomainUserDetails domainUserDetails = userDetailsRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format("Cannot find user with username: %s", username)));
 
-        return new ApplicationUserDetails(domainUserDetails);
+        return new User(domainUserDetails.getEmail(), domainUserDetails.getPassword(), createAuthorities(domainUserDetails.getRoles()));
+    }
+
+    private static Set<SimpleGrantedAuthority> createAuthorities(final Set<UserRole> roles) {
+        return roles.parallelStream()
+                .map(UserRole::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
     }
 }
--- oauth2/multi-module/resource-server/src/main/java/com/github/akraskovski/resource/server/application/config/ResourceServerConfig.java
+++ oauth2/multi-module/resource-server/src/main/java/com/github/akraskovski/resource/server/application/config/ResourceServerConfig.java
@@ -1,12 +1,20 @@
 package com.github.akraskovski.resource.server.application.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * Configuration this module as an OAuth2.0 Resource Server instance.
@@ -18,16 +26,24 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String RESOURCE_ID = "company_api";
 
+    @Bean
+    @ConfigurationProperties(prefix = "auth.datasource")
+    public DataSource authServerDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
     @Override
     public void configure(final ResourceServerSecurityConfigurer resources) {
-        resources.resourceId(RESOURCE_ID);
+        final TokenStore tokenStore = new JdbcTokenStore(authServerDataSource());
+        resources.resourceId(RESOURCE_ID).tokenStore(tokenStore);
     }
 
     @Override
     public void configure(final HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/api/**").authorizeRequests().anyRequest().permitAll()
-//                .antMatchers(HttpMethod.GET, "/**").access("#oauth2.hasScope('read')")
+        http.antMatcher("/api/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/company").access("#oauth2.hasScope('read')")
+                .anyRequest().permitAll()
                 .and()
                 .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
     }
