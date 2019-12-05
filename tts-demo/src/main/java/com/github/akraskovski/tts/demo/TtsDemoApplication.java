1b5b6998ccecf52d67a97aaea650dbd257cb7372
akraskovski <artem.kraskovski@gmail.com>
2019-12-05 21:43:20 +0300

    initial structure for openid-connect application

---
+++ tts-demo/src/main/java/com/github/akraskovski/tts/demo/TtsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.akraskovski.tts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtsDemoApplication.class, args);
+    }
+
+}
---
+++ tts-demo/src/test/java/com/github/akraskovski/tts/demo/TtsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.akraskovski.tts.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
